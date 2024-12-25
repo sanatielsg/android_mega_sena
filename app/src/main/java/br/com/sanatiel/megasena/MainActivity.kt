5d59aed95135111e0739cd17ea18bbb0522b17ec
Sanatiel <sanatielsg@gmail.com>
2024-12-25 00:27:21 -0300

    getting last results before close app

--- app/src/main/java/br/com/sanatiel/megasena/MainActivity.kt
+++ app/src/main/java/br/com/sanatiel/megasena/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.com.sanatiel.megasena
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -11,9 +13,10 @@ import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "logs"
-    lateinit var btnGenerate: Button
-    lateinit var edtNumber: EditText
-    lateinit var txtResult : TextView
+    private lateinit var btnGenerate: Button
+    private lateinit var edtNumber: EditText
+    private lateinit var txtResult : TextView
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         btnGenerate = findViewById(R.id.btn_generate)
         edtNumber = findViewById(R.id.edt_number)
         txtResult = findViewById(R.id.txt_result)
+
+        prefs = getSharedPreferences("shdb", Context.MODE_PRIVATE)
+
+        val oldResult = prefs.getString("result",null)
+        if (oldResult != null) {
+            txtResult.text = oldResult
+        }
 
         btnGenerate.setOnClickListener {
             generateNumbers(edtNumber.text.toString(), txtResult)
@@ -49,5 +59,9 @@ class MainActivity : AppCompatActivity() {
             if (numbers.size == qtyNumbers) break
         }
         textView.text = numbers.joinToString("-")
+
+        val editor = prefs.edit()
+        editor.putString("result",textView.text.toString())
+        editor.apply()
     }
 }
