1662af335a83ef7e7c32393c2d00364dc3e0525b
Sanatiel <sanatielsg@gmail.com>
2024-12-24 23:12:09 -0300

    variables, button click event

--- app/src/main/java/br/com/sanatiel/megasena/MainActivity.kt
+++ app/src/main/java/br/com/sanatiel/megasena/MainActivity.kt
@@ -1,15 +1,37 @@
 package br.com.sanatiel.megasena
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "logs"
+    lateinit var btnGenerate: Button
+    lateinit var edtNumber: EditText
+    lateinit var txtResult : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnGenerate = findViewById(R.id.btn_generate)
+        edtNumber = findViewById(R.id.edt_number)
+        txtResult = findViewById(R.id.txt_result)
+
+        btnGenerate.setOnClickListener {
+            generateNumbers()
+        }
+    }
+
+    fun generateNumbers(){
+        txtResult.text = "Ok"
     }
 }
